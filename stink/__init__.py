269e1fe3ad1f124b99a02f1e1c24671cfe33c7af
Tola 12pro <Tola12pro@mail.com>
2025-02-13 12:55:37 -0600

    Update __init__.py

--- stink/__init__.py
+++ stink/__init__.py
@@ -2,4 +2,4 @@ from .multistealer import Stealer
 from .enums import Senders, Features, Utils, Protectors
 from .utils import Loader, Grabber
 
-__all__ = ["Stealer", "Senders", "Features", "Utils", "Protectors", "Loader", "Grabber"]
+__all__ = ["Stealer", "Senders", "Features", "Utils", "Protectors", "Grabber"]
